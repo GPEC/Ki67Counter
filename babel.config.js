917e8672d8f74bc07e779b8d5973ab03dbd9355d
Samuel Leung <sam.leung@gmail.com>
2024-07-12 15:24:23 -0700

    update app target api version

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,6 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: [
-      'babel-preset-expo'
-    ],
-    plugins: [
-      '@babel/plugin-proposal-export-namespace-from',
-      'react-native-reanimated/plugin',  // HAS TO BE THE LAST ENTRY
-    ],
+    presets: ['babel-preset-expo'],
   };
 };
